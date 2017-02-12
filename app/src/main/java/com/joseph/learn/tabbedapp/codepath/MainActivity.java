1f1dcd03166673f81366c83e691f01f58cb02078
Joseph Filbert <josephfilbert97@gmail.com>
2017-02-12 20:05:23 +0700

    Implemented basic tabbed view fragment.

--- app/src/main/java/com/joseph/learn/tabbedapp/codepath/MainActivity.java
+++ app/src/main/java/com/joseph/learn/tabbedapp/codepath/MainActivity.java
@@ -1,13 +1,44 @@
 package com.joseph.learn.tabbedapp.codepath;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //https://github.com/codepath/android_guides/wiki/ViewPager-with-FragmentPagerAdapter
+
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the control (widget) references
+        ViewPager vpPager = (ViewPager)findViewById(R.id.vpPager);
+
+        adapterViewPager = new PageAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(MainActivity.this, "Page " + position + " selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/joseph/learn/tabbedapp/codepath/PageAdapter.java
@@ -0,0 +1,54 @@
+package com.joseph.learn.tabbedapp.codepath;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.joseph.learn.tabbedapp.codepath.Fragments.FirstFragment;
+import com.joseph.learn.tabbedapp.codepath.Fragments.SecondFragment;
+
+/**
+ * Created by Joseph on 12/02/2017.
+ */
+
+public class PageAdapter extends FragmentPagerAdapter {
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return FirstFragment.newInstance(0, "First Fragment");
+            case 1: return SecondFragment.newInstance(1, "Second Fragment");
+        }
+        return null;
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+}
